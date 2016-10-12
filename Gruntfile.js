5ed268008efa6b36947d3ba5411a1a4c64f396c8
Alan Sangma <asangma@esri.com>
2016-10-12 14:16:04 -0700

    better Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,26 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    sass: {
-      dist: {
-        files: {
-          'css/global-nav.css' : 'scss/global-nav.scss',
-          'demo/css/prototypeChucks.css' : 'demo/scss/prototypeChucks.scss'
+    grunt.initConfig({
+        sass: {
+            options: {
+                outputStyle: 'compressed',
+                sourceMap: false
+            },
+            dist: {
+                files: {
+                    'css/global-nav.css' : 'scss/global-nav.scss',
+                    'demo/css/prototypeChucks.css' : 'demo/scss/prototypeChucks.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['**/*.scss'],
+                tasks: ['sass']
+            }
         }
-      }
-    },
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
 }
