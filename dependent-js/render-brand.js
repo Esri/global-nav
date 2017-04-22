d7a41faf38deb822fccfb3ab33f4f9e8a03cc956
Jonathan Neal <jonathantneal@hotmail.com>
2017-04-21 20:51:16 -0400

    Use SVG logo

--- dependent-js/render-brand.js
+++ dependent-js/render-brand.js
@@ -6,5 +6,10 @@ const prefix = 'esri-gnav-brand';
 
 // render brand element
 export default (brand) => $('a', { class: prefix, href: brand.href }, [
-	$('img', { class: `${ prefix }-image`, ariaLabel: brand.label, src: brand.image })
+	$('svg', {
+		class: `${ prefix }-image`,
+		ariaLabel: brand.label
+	}, [
+		$('use', { 'href': brand.image })
+	])
 ]);
