04cc01275c74084ad1d01180ae6a7cf4f3438875
Jonathan Neal <jonathantneal@hotmail.com>
2017-08-01 10:57:44 -0400

    Add method to selectively enable focus ring

--- shared/shared.js
+++ shared/shared.js
@@ -12,9 +12,45 @@ function $dispatch(target, type, detail) {
 	target.dispatchEvent(event);
 }
 
+function $enableFocusRing(target) {
+	// retooled from https://github.com/jonathantneal/js-focus-ring
+
+	let keyboardThrottleTimeoutID;
+
+	const activeElements = [];
+
+	target.addEventListener('blur', () => {
+		activeElements.forEach((activeElement) => {
+			activeElement.removeAttribute('js-focus');
+			activeElement.removeAttribute('js-focus-ring');
+		});
+	}, true);
+
+	target.addEventListener('focus', () => {
+		const activeElement = document.activeElement;
+
+		if (activeElement instanceof Element) {
+			activeElement.setAttribute('js-focus', '');
+
+			if (keyboardThrottleTimeoutID) {
+				activeElement.setAttribute('js-focus-ring', '');
+			}
+
+			activeElements.push(activeElement);
+		}
+	}, true);
+
+	target.addEventListener('keydown', () => {
+		keyboardThrottleTimeoutID = clearTimeout(keyboardThrottleTimeoutID) || setTimeout(() => {
+			keyboardThrottleTimeoutID = 0;
+		}, 100);
+	}, true);
+}
+
 export {
 	$assign,
 	$dispatch,
+	$enableFocusRing,
 	$fetch,
 	$replaceAll
 };
