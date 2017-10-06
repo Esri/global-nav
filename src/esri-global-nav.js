9702d36a2fca07b726e98aa65f013e17e4385328
Daniel Lewis <daniel_lewis@esri.com>
2017-10-05 20:16:00 -0700

    added in all the wonderful hacks we found while adding this across esri.com

--- src/esri-global-nav.js
+++ src/esri-global-nav.js
@@ -1,5 +1,8 @@
+import base from './base/js/base';
 import createHeader from './header/js/header';
 import createFooter from './footer/js/footer';
+
+base.init();
 
 window.esriHeader = {create: createHeader};
 window.esriFooter = {create: createFooter};
