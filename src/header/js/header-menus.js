04c602a1d67f2c718a338aeb68423cfba201b209
Daniel Lewis <daniel_lewis@esri.com>
2017-10-26 19:44:20 -0700

    Added the hiding of collapsed menus

--- src/header/js/header-menus.js
+++ src/header/js/header-menus.js
@@ -67,7 +67,7 @@ export default ({variant = 'desktop'}) => {
 									class: `${prefix}-link`, id: `${prefix}-${variant}-link-${uuid}-${suuid}`,
 									href: item.href || 'javascript:;' // eslint-disable-line no-script-url
 								},
-								`${item.label}-${variant.substring(0,1).toUpperCase()}`
+								`${item.label}`
 							);
 
 							if (item.data) {
