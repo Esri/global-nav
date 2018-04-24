bab33db8b69a2f39fbcd4add512788b5f7d42221
Matt Priour <mpriour@esri.com>
2018-04-24 18:26:00 -0500

    #27 Provide a way to insert HTML text for branding

--- src/header/js/header-brand.js
+++ src/header/js/header-brand.js
@@ -3,18 +3,25 @@ import {$assign as $, $dispatch, $replaceAll, $renderSvgOrImg} from '../../share
 const prefix = 'esri-header-brand';
 
 export default () => {
-	const $target = $('a', {class: prefix, id: prefix});
-
+	const $target = $('div', {class: `${prefix}-container`});
+	
 	// On Click
 	$target.addEventListener('click', (event) => {
 		$dispatch($target, 'header:click:brand', {event});
 	});
-
+	
 	/* Brand: On Update
 	/* ====================================================================== */
 	$target.addEventListener('header:update:brand', ({detail}) => {
-		$($target, {href: detail.href, aria: {label: detail.label}});
-		$renderSvgOrImg({imgDef: detail.image, imgClass: `${prefix}-image`, imgWidth: detail.width, imgHeight:detail.height, $targetElm:$target});
+		if (detail.image) {
+			const $link = $('a', {class: prefix, id: prefix, href: detail.href, aria: {label: detail.label}});
+			$renderSvgOrImg({imgDef: detail.image, imgClass: `${prefix}-image`, imgWidth: detail.width, imgHeight:detail.height, $targetElm:$link});
+			$($target, $link);
+		}
+		if (detail.brandText) {
+			const $brandText = $('span', {class: `${prefix}-text`}, detail.brandText);
+			$($target, $brandText);
+		}
 	});
 
 	return $target;
