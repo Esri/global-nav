5b66b32ab35a378c2c8962c074e8a78134b87ba0
gabe8788 <gschnall@esri.com>
2018-05-15 13:58:46 -0700

    If no primary apps, don't display app launcher component.

--- src/header/js/header-apps.js
+++ src/header/js/header-apps.js
@@ -625,6 +625,7 @@ export default () => {
 	$target.addEventListener('header:update:apps', ({detail}) => {
 		const $gridIcon = $renderSvgOrImg({imgDef: detail.image.path, imgWidth: detail.image.width, imgHeight: detail.image.height, "shape-rendering": "crispEdges", imgClass: `${prefix} svg-grid-icon`, $targetElm: $appSwitcherIcon});
 
+		if (!detail.primary) return;
 		if (detail.ieVersion) applyDragAndDropAdjustmentsForIE(detail.ieVersion);
 		if (detail.disableDragAndDrop) ddState.disabled = true;
 		if (detail.text) ddState.i18n = detail.text || {};
