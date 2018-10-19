107788336925eb8ac384269e6fab4f0100406586
maccesar <cmarrujo@esri.com>
2018-10-18 21:20:15 -0700

    Adjust responsive styling

--- src/footer/js/footer-breadcrumb.js
+++ src/footer/js/footer-breadcrumb.js
@@ -7,28 +7,24 @@ import {$assign as $} from '../../shared/js/shared';
 /* ========================================================================== */
 
 export default () =>
-	$('div', {class: `esri-footer-breadcrumb`},
+  $('div', {class: `esri-footer-breadcrumb`},
+    $('a', {class: `esri-footer-breadcrumb--pin`}, '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" class="svg-icon"><path d="M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z"/></svg>'),
     $('ul', {class: `esri-footer-breadcrumb--list`},
-      $('li', {class: `esri-footer-breadcrumb--items`},
-        $('a', {class: `esri-footer-breadcrumb--pin`}, '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" class="svg-icon"><path d="M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z"/></svg>')
-      ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/'),
-      $('li', {class: `esri-footer-breadcrumb--items`}, 
+      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
         $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'ArcGIS')
       ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/'),
-      $('li', {class: `esri-footer-breadcrumb--items`}, 
+      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
         $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'Products')
       ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/'),
-      $('li', {class: `esri-footer-breadcrumb--items`}, 
+      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
         $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'Apps for the Field')
       ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/'),
-      $('li', {class: `esri-footer-breadcrumb--items`}, 
+      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
         $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'Survey 123')
       ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/'),
-      $('li', {class: `esri-footer-breadcrumb--items`}, 'Resources'),
-    )
+      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
+        $('p', {class: `esri-footer-breadcrumb--items-current`}, 'Resources'),
+      )
+    ),
+    $('div', {class: 'clear'})
   );
