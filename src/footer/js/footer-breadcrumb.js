79dacb399198f2397fe503cb401a03088764c3ce
maccesar <cmarrujo@esri.com>
2018-10-19 00:18:25 -0700

    Dynamically split path to generate breadcrumb

--- src/footer/js/footer-breadcrumb.js
+++ src/footer/js/footer-breadcrumb.js
@@ -9,22 +9,36 @@ import {$assign as $} from '../../shared/js/shared';
 export default () =>
   $('div', {class: `esri-footer-breadcrumb`},
     $('a', {class: `esri-footer-breadcrumb--pin`}, '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" class="svg-icon"><path d="M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z"/></svg>'),
-    $('ul', {class: `esri-footer-breadcrumb--list`},
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-        $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'ArcGIS')
-      ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-        $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'Products')
-      ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-        $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'Apps for the Field')
-      ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-        $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, 'Survey 123')
-      ),
-      $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-        $('p', {class: `esri-footer-breadcrumb--items-current`}, 'Resources'),
-      )
-    ),
+    $('ul', {class: `esri-footer-breadcrumb--list`}),
     $('div', {class: 'clear'})
   );
+
+
+  window.addEventListener('DOMContentLoaded', () => {
+    // let breadcrumb = window.location.pathname;
+    const breadcrumb = "/en-us/arcgis/products/arcgis-pro/overview";
+    const listWrapper = document.querySelector('.esri-footer-breadcrumb--list');
+
+    if (breadcrumb.length) {
+      const path = breadcrumb.split('/');
+      path.forEach((element, index) => {
+        if (element) {
+          if (listWrapper) {
+            if (index === path.length - 1) {
+              $(listWrapper,
+                $('li', {class: `esri-footer-breadcrumb--items`}, '/',
+                  $('p', {class: `esri-footer-breadcrumb--items-current`}, `${element}`),
+                ),
+              );
+            } else {
+              $(listWrapper,
+                $('li', {class: `esri-footer-breadcrumb--items`}, '/',
+                  $('a', {href: '#', class: `esri-footer-breadcrumb--items-link`}, `${element}`)
+                ),
+              );
+            }
+          }
+        }
+      });
+    }
+  });
