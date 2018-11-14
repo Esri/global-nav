d58176d3890c38f34b27e6e6d163270601b55b38
maccesar <cmarrujo@esri.com>
2018-11-14 00:15:46 -0800

    Code refactoring takes care of requesting changes

--- src/footer/js/footer-breadcrumb.js
+++ src/footer/js/footer-breadcrumb.js
@@ -1,8 +1,9 @@
 /* Global Footer: Tooling
 /* ========================================================================== */
 
-import {$assign as $} from '../../shared/js/shared';
+import {$assign as $, $renderSvgOrImg} from '../../shared/js/shared';
 
+import {$pin} from '../../shared/js/iconPaths';
 /* Global Footer Breadcrumb
 /* ========================================================================== */
 
@@ -34,7 +35,7 @@ export default (data, prefix) => {
     }
 
     return $('div', {class: `${prefix}-breadcrumb`},
-      $('a', {class: `${prefix}-breadcrumb--pin`}, '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" class="svg-icon"><path d="M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z"/></svg>'),
+      $('a', {class: `${prefix}-breadcrumb--pin`}, $renderSvgOrImg({imgDef: $pin.sm, imgClass: `svg-icon`, imgWidth:"17", imgHeight:"17", viewBox:"0 0 32 32"})),
       listWrapper,
       $('div', {class: 'clear'})
     );
