ddb98c8fd6ee93367e7ae6782902d53ed135eaa5
maccesar <cmarrujo@esri.com>
2019-01-31 10:23:37 -0800

    Requested changes for PR

--- src/footer/js/footer-breadcrumb.js
+++ src/footer/js/footer-breadcrumb.js
@@ -1,21 +1,21 @@
 /* Global Footer: Tooling
 /* ========================================================================== */
 
-import {$assign as $} from '../../shared/js/shared';
+import {$assign as $, $renderSvgOrImg} from '../../shared/js/shared';
 
 /* Global Footer Breadcrumb
 /* ========================================================================== */
-
+const prefix =  'esri-footer-breadcrumb';
 export default () =>
-  $('div', {class: `esri-footer-breadcrumb`},
-    $('a', {href: 'https://www.esri.com', class: `esri-footer-breadcrumb--pin`}, '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" class="svg-icon"><path d="M15.999 0C11.214 0 8 1.805 8 6.5v17l7.999 8.5L24 23.5v-17C24 1.805 20.786 0 15.999 0zM16 14.402A4.4 4.4 0 0 1 11.601 10a4.4 4.4 0 1 1 8.798 0A4.4 4.4 0 0 1 16 14.402z"/></svg>'),
-    $('ul', {class: `esri-footer-breadcrumb--list`}),
-    $('div', {class: 'clear'})
+  $('div', {class: `${prefix}`},
+    $('a', {href: 'https://www.esri.com', class: `${prefix}--pin`}),
+    $('ul', {class: `${prefix}--list`}),
+    $('div', {class: 'clearFloats'})
   );
 
-
   window.addEventListener('DOMContentLoaded', () => {
-    const breadcrumb = window.location.pathname;
+    // const breadcrumb = window.location.pathname;
+    const breadcrumb = 'en-us/industries/petroleum/overview';
     const listWrapper = document.querySelector('.esri-footer-breadcrumb--list');
 
     const rules = [
@@ -69,23 +69,21 @@ export default () =>
       newPath.forEach((element, index) => {
         const crumb = validateRule(element);
         const url = buildLinkURL(newPath, index);
-        if (element) {
-          if (listWrapper) {
-            const currentPage = element.replace(/-/gi, ' ').replace(/\.html/gi, '');
-            const crumbPage = crumb.replace(/-/gi, ' ').replace(/\.html/gi, '');
-            if (index === newPath.length - 1) {
-              $(listWrapper,
-                $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-                  $('p', {class: `esri-footer-breadcrumb--items-current`}, `${currentPage}`),
-                ),
-              );
-            } else {
-              $(listWrapper,
-                $('li', {class: `esri-footer-breadcrumb--items`}, '/',
-                  $('a', {href: url, class: `esri-footer-breadcrumb--items-link`}, `${crumbPage}`)
-                ),
-              );
-            }
+        if (element && listWrapper) {
+          const currentPage = element.replace(/-/gi, ' ').replace(/\.html/gi, '');
+          const crumbPage = crumb.replace(/-/gi, ' ').replace(/\.html/gi, '');
+          if (index === newPath.length - 1) {
+            $(listWrapper,
+              $('li', {class: `${prefix}--items`}, '/',
+                $('p', {class: `${prefix}--items-current`}, `${currentPage}`),
+              ),
+            );
+          } else {
+            $(listWrapper,
+              $('li', {class: `${prefix}--items`}, '/',
+                $('a', {href: url, class: `${prefix}--items-link`}, `${crumbPage}`)
+              ),
+            );
           }
         }
       });
