20763aaf88889ef381316f21a14bb9cfe2e16bfb
maccesar <cmarrujo@esri.com>
2020-01-22 00:55:03 -0800

    Adjust sizing for single columns menus

--- src/header/js/header-menus.js
+++ src/header/js/header-menus.js
@@ -177,7 +177,7 @@ export default ({variant = 'desktop'}) => {
 
 								if (hasMenuItems) {
 									const total = item.menus.length;
-									if (total > 10) {
+									if (total >= 10) {
 										hasMultiCols = total % 3 === 0;
 										columns = Math.min(Math.ceil(total / 9), 3);
 									}
